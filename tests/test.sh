bcd40641c689a96c8c7de3895d8c85d24b02c60e
andyexeter <palmer.andy@gmail.com>
2015-12-06 16:47:56 +0000

    Fix netbeans formatting errors

--- tests/test.sh
+++ tests/test.sh
@@ -3,19 +3,23 @@
 clear
 files=$(find ../ -mindepth 1 -maxdepth 2 -type f -name '*.php')
 
-echo "Removing any surrounding spaces between array keys that contain a string or an integer...\n"
+echo "Removing any surrounding spaces between array keys that contain a string or an integer..."
 
 for i in $files; do
 	echo "Formatting $i\n"
 	cp $i $i.orig
 	# This regex replaces [ 0 ] with [0], [ 123 ] with [123], [ "ID" ] with ["ID"] and [ 'ID' ] with ['ID']
 	sed -ri "s/\[[ ]*([0-9]+|[\x27|\x22][^\x27|\x22]*[\x27|\x22])[ ]*\]/[\1]/g" $i
-	sed -ri "s/\[ \]/[\1]/g" $i
+	# Replace [ ] with []
+	sed -ri "s/\[ \]/[]/g" $i
+	# Replace description coming soon.
+	sed -ri "s/ \[[ ]*([0-9]+|[\x27|\x22][^\x27|\x22]*[\x27|\x22])[ ]*\]/ [ \1 ]/g" $i
+	sed -ri "s/ \[([^ ].*) \]/ [ \1 ]/g" $i
 	# Delete the backup if no changes were made
 	cmp --silent post-lockdown.php post-lockdown.php.orig && rm -f $i.orig
 done
 
-echo "Running PHP Linter...\n"
+echo "Running PHP Linter..."
 
 for i in $files; do
 	php -l $i
@@ -23,7 +27,7 @@ done
 
 STANDARD="WordPress-Extra"
 
-echo "Running PHPCodeSniffer with $STANDARD standards...\n"
+echo "Running PHPCodeSniffer with $STANDARD standards..."
 
 ERRORS=0
 for i in $files; do
